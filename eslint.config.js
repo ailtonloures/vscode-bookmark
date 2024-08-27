47d2460ff680081f8d775a0860c6b571e0b296ec
Ailton Loures Ferreira <ailton.ferreira@stoneage.com.br>
2024-08-27 10:01:21 -0300

    build: struturing project

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+	{ languageOptions: { globals: globals.node } },
+	pluginJs.configs.recommended,
+	{
+		ignores: ['node_modules'],
+	},
+	eslintConfigPrettier,
+];
