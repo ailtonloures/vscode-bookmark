3133d9a6d31c46f52e6784719e8755ddfe4d74a7
Ailton Loures <dev.ailtonloures@proton.me>
2024-09-14 16:54:03 -0300

    fix: main

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import squirrelStartup from 'electron-squirrel-startup';
 import { app } from 'electron/main';
 import { renderTray } from './tray.js';
 
-if (squirrelStartup || !app.requestSingleInstanceLock()) {
-	app.quit();
-}
+if (squirrelStartup || !app.requestSingleInstanceLock()) app.quit();
 
 Sentry.init({
 	dsn: 'https://713782327975276ae010040b1db6ab8a@o4507887084503040.ingest.us.sentry.io/4507887098724352',
