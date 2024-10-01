8005f8bf53c4d7d6d6f3fa4d293e67466590d9ac
Ailton Loures <dev.ailtonloures@proton.me>
2024-10-01 11:24:29 -0300

    build: removing vite-plugin

--- vite.main.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config
-export default defineConfig({
-	resolve: {
-		browserField: false,
-		mainFields: ['module', 'jsnext:main', 'jsnext'],
-	},
-});
