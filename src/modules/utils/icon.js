e1a6371d331df137ef6f79bcc5e9f1752f04db48
Ailton Loures <dev.ailtonloures@proton.me>
2024-10-26 19:04:41 -0300

    fix: fixing icons path

--- src/modules/utils/icon.js
+++ src/modules/utils/icon.js
@@ -1,28 +1,10 @@
-import { app, nativeImage } from 'electron';
+import { nativeImage } from 'electron';
 import { resolve } from 'node:path';
 
-import { isDevMode } from './vite';
-
 function getIcon(iconName) {
-	let iconPath = resolve(
-		app.getAppPath(),
-		'app.asar',
-		'icons',
-		'main',
-		iconName
+	return nativeImage.createFromPath(
+		resolve(__dirname, 'icons', 'main', iconName)
 	);
-
-	if (isDevMode()) {
-		iconPath = resolve(
-			app.getAppPath(),
-			'public',
-			'icons',
-			'main',
-			iconName
-		);
-	}
-
-	return nativeImage.createFromPath(iconPath);
 }
 
 export { getIcon };
